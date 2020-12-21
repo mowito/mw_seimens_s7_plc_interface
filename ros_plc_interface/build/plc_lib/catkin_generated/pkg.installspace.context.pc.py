a09228ea4fb4b58a027de5e7139a5fd868798838
ankurbodhe <ankurbodhe@gmail.com>
2020-12-21 23:10:37 +0530

    Updated Odometry calculations

--- ros_plc_interface/build/plc_lib/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "plc_lib"
-PROJECT_SPACE_DIR = "/home/ankur/Documents/rucha_mowito/mw_seimens_s7_plc_interface/ros_plc_interface/install"
-PROJECT_VERSION = "0.0.0"
